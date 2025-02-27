9b085efb2628052e5d7884087915906c782e71ed
M4RT1NQU1N0N3S <158176431+M4RT1NQU1N0N3S@users.noreply.github.com>
2025-02-26 19:58:21 -0500

    Cargar Clase Cliente

--- ProyectoTest2/src/Beans/Cliente.java
+++ ProyectoTest2/src/Beans/Cliente.java
@@ -9,5 +9,59 @@ package Beans;
  * @author quino
  */
 public class Cliente {
+    private int idCliente;
+    private String nombreCliente;
+    private String correoCliente;
+    private String telefono;
+    private String codPais;
+
+    public Cliente(int idCliente, String nombreCliente, String correoCliente, String telefono, String codPais) {
+        this.idCliente = idCliente;
+        this.nombreCliente = nombreCliente;
+        this.correoCliente = correoCliente;
+        this.telefono = telefono;
+        this.codPais = codPais;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getCorreoCliente() {
+        return correoCliente;
+    }
+
+    public void setCorreoCliente(String correoCliente) {
+        this.correoCliente = correoCliente;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCodPais() {
+        return codPais;
+    }
+
+    public void setCodPais(String codPais) {
+        this.codPais = codPais;
+    }
+    
     
 }
